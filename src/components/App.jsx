0845abbc13c5e0afd2d28aeea60ba0b6fc8a4162
deepikayadav14 <deepikayadav.cse20@ggct.co.in>
2022-09-28 00:23:40 -0700

    no update

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Note";
+
+function App(){
+    return <div>
+        <Header />
+        <Notes />
+        <Footer />
+    </div>
+}
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes(){
+    return(
+        <div className="note">
+            <h1>This is my heading.</h1>
+            <p>This is my content.</p>
+        </div>
+
+    );
+}
+export default Notes;
